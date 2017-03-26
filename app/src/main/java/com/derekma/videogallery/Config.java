11a49f6f6623672bde644519eee34b0aaaece078
DanielStanlee <danielstanlee16@gmail.com>
2017-03-27 00:29:49 +0530

    Initial commit

---
+++ app/src/main/java/com/derekma/videogallery/Config.java
@@ -0,0 +1,30 @@
+package com.derekma.videogallery;
+
+/**
+ * Created by derekma on 16/2/1.
+ * A class provides API key and urls for downloading data from API services.
+ */
+public final class Config {
+
+    private Config() {
+
+    }
+
+    /**
+     * A static final variable.
+     * Youtube API Key.
+     */
+    public static final String YOUTUBE_API_KEY = "AIzaSyArBYwU9byCpZmAx2wnAtn2EEtZLRX-rG4";
+
+    /**
+     * A static final variable.
+     * YouTube popular videos API.
+     */
+    public static final String YOUTUBE_API_URL = "your api";
+
+    /**
+     * A static final variable.
+     * NPR news latest news API.
+     */
+    public static final String NPR_NEWS_API = "your api";
+}
